58c6e5981ae5c2a65d9e7d2cd9177cd785bfd2cc
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-07-07 16:51:47 +0200

    update exercise object/methods

---
+++ exercise/es-23/exercise.js
@@ -0,0 +1,21 @@
+const person = {
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+}
+
+
+for (const keys in person) {
+  console.log(keys, person[keys]);
+
+}
+
+
+
+
+
+
+
+
+
+// Print values of person using Object.keys
