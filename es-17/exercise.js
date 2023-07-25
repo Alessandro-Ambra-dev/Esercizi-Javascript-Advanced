8d0f8b726966c8b4c8f9b3a6e0802d20972be531
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-07-25 19:46:26 +0200

    feature: exercise promise chaining done

--- es-17/exercise.js
+++ es-17/exercise.js
@@ -1,23 +1,27 @@
 const isLogged = true;
 
-let isUserLogged = new Promise((resolve, reject) => {
-  if (isLogged == true) {
-    resolve(Math.random());
-  } else {
-    reject(new Error("User not logged in"));
-  }
-});
+function isUserLogged(isLogged) {
+  return new Promise((resolve, reject) => {
+    let number = Math.random();
+    if (isLogged == true) {
+      resolve(number);
+    } else {
+      reject(new Error("User not logged in"));
+    }
+  });
+}
 
-let isNumberOk = new Promise((resolve, reject) => {
-  let n = Math.random();
-  if (n > 0.5) {
-    resolve(console.log({ name: "John", age: 24 }));
-  } else {
-    reject(new Error("Number is too low"));
-  }
-});
+function isNumberOk(number) {
+  return new Promise((resolve, reject) => {
+    if (number > 0.5) {
+      resolve({ name: "John", age: "24" });
+    } else {
+      reject(new Error("Number is too low"));
+    }
+  });
+}
 
-isUserLogged
+isUserLogged(true)
   .then(isNumberOk)
   .then((val) => console.log(val))
   .catch((err) => console.error(err));
