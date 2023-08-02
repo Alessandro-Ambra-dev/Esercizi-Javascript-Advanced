e6933381125e54af2c7229f4b940327aa90244f8
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-08-02 17:13:31 +0200

    feature: exercise 1 of local storage done

--- es-21/exercise.js
+++ es-21/exercise.js
@@ -8,6 +8,11 @@ function saveUser() {
   localStorage.setItem("My obj", JSON.stringify(user));
   let object = localStorage.getItem("My obj");
   console.log(JSON.parse(object));
+  let par = document.createElement("p");
+  let content = document.createTextNode(object);
+  let body = document.querySelector("body");
+  par.append(content);
+  body.appendChild(par);
 }
 
 saveUser();
