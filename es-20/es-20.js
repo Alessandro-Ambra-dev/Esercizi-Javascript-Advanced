a94183e5348dc674b6347ba798c002cbdac55a43
Alessandro Ambra <alessandro1998.aa@gmail.com>
2023-08-02 15:34:13 +0200

    feat: network-request es 3 done

--- es-20/es-20.js
+++ es-20/es-20.js
@@ -1,25 +1,27 @@
 
 
-// async function buttonClick(){
-//     let request = await fetch("https://jsonplaceholder.typicode.com/posts", {method: "post"});
-//     let inp = document.getElementById("todo-title");
-//     let inp2 = document.getElementById("todo-completed");
-//     if(inp.lenght > 1 && inp2.cheked == true){
+async function buttonClick(){
+    let request = await fetch("https://jsonplaceholder.typicode.com/posts", {method: "post"});
+    let inp = document.getElementById("todo-title");
+    let inp2 = document.getElementById("todo-completed");
+    if(inp.lenght > 1 && inp2.cheked == true){
 
-//     }
-// }
+    }
+}
+let button = document.getElementById("submit-todo");
+button.addEventListener("click", checkForm())
 async function checkForm(){
-    let form = document.querySelector("form");
     let input = document.getElementById("todo-title");
     let input2 = document.getElementById("todo-completed");
-    let button = document.getElementById("submit-todo");
     let response =  await fetch("https://jsonplaceholder.typicode.com/posts", {
         method: "POST",
-        body: "title and Flag completed"
+        body: {
+            title: input.value,
+            toDO: input2.checked
+        }
     });
-    form.appendChild(input, input2, button)
-    let text = button.createTextNode(response)
-    button.click(text)
+    console.log(response);
 }
 
-console.log(checkForm)
+// console.log(checkForm())
+
